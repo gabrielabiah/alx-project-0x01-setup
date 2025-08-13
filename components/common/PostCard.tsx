e3b0fe76567f2fa5053b6985e342f45e3cffa340
gabrielabiah <gabriel@manifestghana.com>
2025-08-13 23:10:44 +0000

    initial commit

---
+++ components/common/PostCard.tsx
@@ -0,0 +1,5 @@
+const PostCard = () => {
+	return <div className="bg-white p-4 rounded-md">Post Card</div>;
+};
+
+export default PostCard;
---
+++ components/layout/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+	return <div className="bg-gray-200 p-4">Footer</div>;
+};
+
+export default Footer;
