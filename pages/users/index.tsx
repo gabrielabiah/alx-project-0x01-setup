2e39300a76108165d185d385310ba25ccba705c9
gabrielabiah <gabriel@manifestghana.com>
2025-08-16 12:19:07 +0000

    implemented posts card content

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,5 +1,14 @@
+import Header from "@/components/layout/Header";
+
 const Users = () => {
-	return <div>Users</div>;
+	return (
+		<div className="flex flex-col h-screen">
+			<Header />
+			<main>
+				<h1>Users</h1>
+			</main>
+		</div>
+	);
 };
 
 export default Users;	
